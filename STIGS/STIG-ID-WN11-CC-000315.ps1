ca1f8068cea3d56f464091c97fca0e24ac43ae25
Travis Young <travis.d.young@gmail.com>
2026-06-03 18:24:53 -0400

    Update and rename STIG-ID-WN11-CC-000315).ps1 to STIG-ID-WN11-CC-000315.ps1

--- STIGS/STIG-ID-WN11-CC-000315.ps1
+++ STIGS/STIG-ID-WN11-CC-000315.ps1
@@ -6,7 +6,7 @@
     Author          : Travis Young
     LinkedIn        : https://www.linkedin.com/in/travis-young-516156102/
     GitHub          : https://github.com/YoungTravis03
-    Date Created    : 2024-05-31
+    Date Created    : 2026-05-31
     Last Modified   : 2064-09-09
     Version         : 1.0
     CVEs            : N/A
